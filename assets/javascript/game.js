cf9d0996f30793635d57f82b3a70285b2cbe2a20
Paola L <paolalarrazabal@gmail.com>
2019-10-06 08:10:33 -0400

    added script for js at the bottom of body for to add from other file, had to change file because i spelled psychic incorrectly, removed document.ready from beginning of js file because it was not working, removed it and js started working again.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+
 
 
 const computerGuessOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -51,4 +51,3 @@ function starOver () {
 
 }
 
-});
